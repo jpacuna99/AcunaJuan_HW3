3b31d52dbce4de49ec081eb2523a618d89c30ca0
Juan Pablo Acuña Acosta <UANDES\jp.acuna@fisicaez12204l.ad.uniandes.edu.co>
2018-10-25 18:40:18 -0500

    4

--- Fourier2D.py
+++ Fourier2D.py
@@ -1,9 +1,18 @@
 import Image
 import numpy as np
 from scipy.fftpack import fft2, ifft2
+from scipy import fftpack, ndimage
 import matplotlib.pyplot as plt
-pilimg = Image.open("arbol(1).png")
+
+image = ndimage.imread("arbol(1).png") 
 
 
-arbol = np.array(pilimg)
-print np.shape(arbol)
+arbol = np.array(image)
+fourier2=np.fft.fft2(arbol)
+freq = np.fft.fftfreq(len(arbol[0]))
+
+plt.imshow(np.log10(abs(fourier2)))
+plt.figure()
+plt.plot(freq,np.log10(abs(fourier2[:,0])))
+plt.show()
+print arbol[0,0]
