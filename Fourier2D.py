e60676380837a141b4dff21bf1f740a4a391f8b8
Juan Pablo Acuña Acosta <UANDES\jp.acuna@fisicaez12206l.ad.uniandes.edu.co>
2018-10-26 11:36:11 -0500

    5

--- Fourier2D.py
+++ Fourier2D.py
@@ -3,16 +3,47 @@ import numpy as np
 from scipy.fftpack import fft2, ifft2
 from scipy import fftpack, ndimage
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 
-image = ndimage.imread("arbol(1).png") 
+image = plt.imread("arbol(1).png") 
 
 
-arbol = np.array(image)
-fourier2=np.fft.fft2(arbol)
-freq = np.fft.fftfreq(len(arbol[0]))
 
-plt.imshow(np.log10(abs(fourier2)))
+fourier2=fft2(image)
+#freq = np.fft.fftfreq(len(arbol[0]))
 plt.figure()
-plt.plot(freq,np.log10(abs(fourier2[:,0])))
+plt.imshow(np.log10(abs(fourier2)),plt.cm.gray)
+plt.colorbar()
+
+for i in range(np.shape(fourier2)[0]):
+	for j in range(np.shape(fourier2)[1]):	
+		if np.log10(abs(fourier2[i,j]))>3.613:
+			fourier2[i,j]=0
+
+
+
+plt.figure()
+plt.imshow(np.log10(abs(fourier2)),plt.cm.gray)
+plt.colorbar()
+plt.figure()
+arbol=ifft2(fourier2)
+
+plt.imshow(abs(arbol),plt.cm.gray)
+
+
+
+
 plt.show()
 print arbol[0,0]
+
+
+
+
+
+
+
+
+
+
+
+
