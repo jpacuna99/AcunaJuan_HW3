6cfc4c6d275381b2544ef0b689a7abaef1a9cb7b
Juan Pablo Acuña Acosta <UANDES\jp.acuna@fisicaez12205l.ad.uniandes.edu.co>
2018-10-29 19:04:04 -0500

    10

--- Fourier2D.py
+++ Fourier2D.py
@@ -10,23 +10,31 @@ image = plt.imread("arbol(1).png")
 
 
 fourier2=fft2(image)
-#freq = np.fft.fftfreq(len(arbol[0]))
+freq = np.fft.fftfreq(len(fourier2[0]))
+freq1 = np.fft.fftfreq(len(fourier2[1]))
+
+
+
+
 plt.figure()
-plt.imshow(np.log10(abs(fourier2)),plt.cm.gray)
+plt.imshow(np.log10(abs(np.fft.fftshift(fourier2))),plt.cm.gray)
 plt.colorbar()
 
+
+filtro=np.copy(fourier2) 
 for i in range(np.shape(fourier2)[0]):
 	for j in range(np.shape(fourier2)[1]):	
-		if np.log10(abs(fourier2[i,j]))>3.613:
-			fourier2[i,j]=0
+		if abs(filtro[i,j])>4100 and abs(fourier2[i,j])<4600 :
+			filtro[i,j]=10**-10
 
 
 
 plt.figure()
-plt.imshow(np.log10(abs(fourier2)),plt.cm.gray)
+plt.imshow(np.log10(abs(np.fft.fftshift(filtro))),plt.cm.gray)
 plt.colorbar()
+
 plt.figure()
-arbol=ifft2(fourier2)
+arbol=ifft2(filtro)
 
 plt.imshow(abs(arbol),plt.cm.gray)
 
@@ -34,7 +42,6 @@ plt.imshow(abs(arbol),plt.cm.gray)
 
 
 plt.show()
-print arbol[0,0]
 
 
 
